5844f22b22f228b7e6f970ae8b2362181c467f17
uc-aeg <akansha.george@ucertify.com>
2021-06-14 13:32:58 -0400

    commit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import InfoCircle from '../common/InfoCircle';
 import NotificationMenuComponent from '../common/NotificationMenuComponent';
 import UserDropdownMenu from '../common/UserDropdownMenu';
@@ -9,20 +9,32 @@ const Navbar = () => {
     const [openinfomenu, setOpenInfoMenu] = useState(false);
     const [openbellicon, setOpenBellIcon] = useState(false);
 
-    const toggle = () => {
-        setOpen(!open);
-    }
+    const wrapperRef = useRef(null);
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+                setOpen(open);
+                setOpenInfoMenu(openinfomenu);
+                setOpenBellIcon(openbellicon);
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [wrapperRef]);
 
     return (
         <nav className="navbar">
             <div className="navbar__right">
                 <button className="btn btn-primary">Button1</button>
-                <i className="fas fa-info-circle" style={{ fontStyle: "italic" }} onClick={() => setOpenInfoMenu(!openinfomenu)}></i>
+                <i className="fas fa-info-circle" style={{ fontStyle: "italic" }} onClick={() => setOpenInfoMenu(!openinfomenu)} ref={wrapperRef}></i>
                 <InfoCircle show={openinfomenu} />
-                <i className="fas fa-bell" onClick={() => setOpenBellIcon(!openbellicon)}></i>
+                <i className="fas fa-bell" onClick={() => setOpenBellIcon(!openbellicon)} ref={wrapperRef}></i>
                 <NotificationMenuComponent show={openbellicon} />
                 <i className="fas fa-user"></i>
-                <span style={{ marginLeft: '-9px' }} className="dropdown-toggle" data-toggle="dropdown" onClick={() => toggle(!open)}>Jason Statham<span className="caret"></span></span>
+                <span style={{ marginLeft: '-9px' }} className="dropdown-toggle" data-toggle="dropdown" onClick={() => setOpen(!open)} ref={wrapperRef}>Jason Statham<span className="caret"></span></span>
                 <UserDropdownMenu open={open} />
             </div>
         </nav>
